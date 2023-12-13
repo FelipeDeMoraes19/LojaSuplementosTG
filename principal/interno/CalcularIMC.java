f75861a99404473835d12d6473b6c4b3b3197630
Felipe de Moraes <moorraes16@gmail.com>
2023-12-13 14:50:43 -0300

    Classe Main Implementada

--- principal/interno/CalcularIMC.java
+++ principal/interno/CalcularIMC.java
@@ -1,6 +1,6 @@
 package principal.interno;
 
-public class CalculadoraIMC {
+public class CalcularIMC {
     public static double calcularIMC(double peso, double altura) {
         double imc = peso / (altura * altura);
         return imc;
